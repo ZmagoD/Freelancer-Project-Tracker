d3382804bc319a0253836f60e7a9e9b1a570f8d7
zmagoD <zmago_devetak@yahoo.com>
2018-05-20 21:35:40 +0200

    fixes name conflicts

--- app/javascript/packs/week-planner-bundle.js
+++ app/javascript/packs/week-planner-bundle.js
@@ -4,5 +4,5 @@ import WeekPlannerApp from '../bundles/WeekPlanner/startup/WeekPlannerApp';
 
 // This is how react_on_rails can see the WeekPlanner in the browser.
 ReactOnRails.register({
-  HelloWorldApp: WeekPlannerApp,
+  WeekPlannerApp: WeekPlannerApp,
 });
